a4e5a4b7d8123594360e0c876479b2215e04b9d5
shafanaura <shafanaura48@gmail.com>
2021-05-01 14:43:01 +0700

    rename & change app icon

--- android/app/src/main/java/com/foodapp/MainActivity.java
+++ android/app/src/main/java/com/foodapp/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     SplashScreen.show(this);
-    return "foodApp";
+    return "TokoKu";
   }
 }
